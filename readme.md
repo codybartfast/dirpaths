3f4a59021e67ceb1e74c89eabb1d1f7b1a399b21
John Donnellan <john@codybartfast.com>
2022-05-29 08:04:31 +0100

    Initial Commit

---
+++ readme.md
@@ -0,0 +1,1 @@
+DirPaths seemed like a good idea at 2022-05-29T07:04:31.3853899Z.
---
+++ tests/DirPaths.Test/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace DirPaths.Test;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
