255d0384947d0de083ee26988d245ce9ab11299d
John Donnellan <john@codybartfast.com>
2022-05-29 09:24:14 +0100

    Play with DirPath

---
+++ src/DirPaths/DirPaths.cs
@@ -0,0 +1,9 @@
+namespace Bmfm;
+
+public class DirPaths{
+    public static void XXX(){
+        var p = new DirPath("Banana");
+        p.Path = "Fred";
+        System.Console.WriteLine($"Bye: {p.Path}");
+    }
+}
