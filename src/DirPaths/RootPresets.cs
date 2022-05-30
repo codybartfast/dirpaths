2de455fba0ce9a87854e53e8ce142b2928bb960b
John Donnellan <john@codybartfast.com>
2022-05-30 14:28:54 +0100

    reorder members

--- src/DirPaths/RootPresets.cs
+++ src/DirPaths/RootPresets.cs
@@ -4,23 +4,23 @@ public static class RootPresets
 {
     public const string DefaultEnvironmentVariableName = "FMBM_APPROOT";
 
-    public static string? AnyBinAppRootSibling()
-    {
-        return RootPresets.Base.SearchUp("bin").Sibling("AppRoot");
-    }
-
     public static string Base { get; } = AppContext.BaseDirectory;
-
-    public static string? BinParent()
-    {
-        return RootPresets.Base.NameIs("bin").Parent();
-    }
 
     public static string Current => Directory.GetCurrentDirectory();
 
     public static string? EnvironmentVariable(string name = DefaultEnvironmentVariableName)
     {
         return Environment.GetEnvironmentVariable(name);
+    }
+
+    public static string? BinParent()
+    {
+        return RootPresets.Base.NameIs("bin").Parent();
+    }
+
+    public static string? AnyBinAppRootSibling()
+    {
+        return RootPresets.Base.SearchUp("bin").Sibling("AppRoot");
     }
 
     public static string? Fmbm(string environmentVariableName = DefaultEnvironmentVariableName)
